c8a8e299c04e00525cc2b7a70fd65fb6f61983b2
kimcoolming <jaizen73@naver.com>
2025-04-27 20:53:02 +0900

    메인페이지 수정

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>My Homepage-coolming</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text to="/">Home</v-btn>
+      <v-btn text to="/about">About</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
